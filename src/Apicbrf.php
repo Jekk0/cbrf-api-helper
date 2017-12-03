7fa523439c4839067f7c878c405ad83ecb9bdc17
Eugene Gubanov <eugene.gubanov@extreme-idea.com>
2017-12-03 21:38:32 +0200

    test commit

--- src/Apicbrf.php
+++ src/Apicbrf.php
@@ -4,7 +4,7 @@ namespace Jekk0\Apicbrf;
 
 
 class Apicbrf {
-    
+
     protected $curl;
     
     public function __construct() {
@@ -13,6 +13,10 @@ class Apicbrf {
 
     public function getCurrency($currency) {
         
+    }
+
+    public function getAllCurrencies($date = NULL) {
+        return $this->curl->get(ApicbrfConstants::ALL_CURRENCIES_QUOTATIONS_URL);
     }
 
 }
---
+++ src/ApicbrfConstants.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Jekk0\Apicbrf;
+
+
+class ApicbrfConstants {
+    const ALL_CURRENCIES_QUOTATIONS_URL = "http://www.cbr.ru/scripts/XML_daily.asp";
+
+    const CURRENCY_DYNAMICS_QUOTATIONS_URL = "http://www.cbr.ru/scripts/XML_dynamic.asp";
+
+    const DYNAMICS_QUOTATIONS_METAL_URL = "http://www.cbr.ru/scripts/xml_metall.asp";
+}
