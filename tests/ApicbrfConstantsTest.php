0b03b3936b0b3c6c20d5008b018b5bec2b3df3f1
Eugene Gubanov <eugene.gubanov@extreme-idea.com>
2017-12-04 22:07:08 +0200

    Configure tests

---
+++ tests/ApicbrfConstantsTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * PHP wrapper for Central Bank Of Russian Federation Api.
+ *
+ * @package    Jekk0/cbrf-api-helper
+ * @author     Jekko https://github.com/Jekk0
+ * @license    MIT
+ * @link       https://github.com/Jekk0/cbrf-api-helper
+ */
+namespace Jekk0\Apicbrf;
+
+/**
+ * Class ApicbrfConstants
+ * @package Jekk0\Apicbrf
+ */
+class ApicbrfConstantsTest extends \PHPUnit\Framework\TestCase {
+
+    public function testCheckConstantsType() {
+        $constants = array(
+            ApicbrfConstants::DATE_FORMAT,
+            ApicbrfConstants::ALL_CURRENCIES_QUOTATIONS_URL,
+            ApicbrfConstants::ALL_CURRENCIES_QUOTATIONS_DATE,
+            ApicbrfConstants::CURRENCY_DYNAMICS_QUOTATIONS_URL,
+            ApicbrfConstants::CURRENCY_DYNAMICS_QUOTATIONS_DATE1,
+            ApicbrfConstants::CURRENCY_DYNAMICS_QUOTATIONS_DATE2,
+            ApicbrfConstants::CURRENCY_DYNAMICS_QUOTATIONS_CURRENCY_ID,
+            ApicbrfConstants::DYNAMICS_QUOTATIONS_METAL_URL,
+            ApicbrfConstants::METAL_DYNAMICS_QUOTATIONS_DATE1,
+            ApicbrfConstants::METAL_DYNAMICS_QUOTATIONS_DATE2
+        );
+        foreach ($constants as $value) {
+            $this->assertTrue(is_string($value));
+        }
+    }
+}
---
+++ tests/CurlInstanceTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * PHP wrapper for Central Bank Of Russian Federation Api.
+ *
+ * @package    Jekk0/cbrf-api-helper
+ * @author     Jekko https://github.com/Jekk0
+ * @license    MIT
+ * @link       https://github.com/Jekk0/cbrf-api-helper
+ */
+namespace Jekk0\Apicbrf;
+
+/**
+ * Class CurlInstance
+ * @package Jekk0\Apicbrf
+ */
+class CurlInstanceTest extends \PHPUnit\Framework\TestCase {
+
+}
